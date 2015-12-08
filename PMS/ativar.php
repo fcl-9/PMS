cac43ad9610949929a95a6da6f5dc1433a6e7a7c
Fabio <2053913@student.uma.pt>
2015-12-08 11:23:19 +0000

    Reserva
    [BUG] gerar mesa aleatório caso o cliente não escolha.
    [BUG] o envio de emails não suporta utf 8 os emails ficam todos
    estranhos com carateres esquesitos.

--- PMS/ativar.php
+++ PMS/ativar.php
@@ -1,5 +1,8 @@
 <?php
 	require_once('/common/database.php');
+	require_once('/common/common.php');
+mysqli_autocommit($link,false);
+$flag = true;
 
 	$id = $_GET['id'];
 	$emailMd5 = $_GET['email'];
@@ -21,7 +24,7 @@
 				if(!$result)
 				{
 					echo 'Erro ao executar query #8'. mysqli_error($link);
-					die;
+					mysqli_rollback($link);
 				}
 				$controlVar = true;
 				//Verifica se o id existe
@@ -35,8 +38,8 @@
 				{
 					$controlVar = false;
 				}
-
-				if(md5($result['email']) != $emailMd5)
+				$emailSendInfo = $result['email'];
+				if(md5($emailSendInfo) != $emailMd5)
 				{
 					$controlVar = false;
 				}
@@ -48,22 +51,52 @@
 					if(!$result)
 					{
 						echo 'Erro ao executar query #9'. mysqli_error($link);
-						die;
+						mysqli_rollback($link);
 					}
 					else
 					{
 						echo 'Reserva confirmada.';
+						//Gerar nova password e enviar por mail a password a hora o dia 
+						$password = random_password(8);
+						$updatePassword = 'UPDATE cliente SET password = '.$password.' WHERE idcliente = '.$id.'';	
+						$sucessPwd = mysqli_query($link,$updatePassword);
+						if($sucessPwd)
+						{
+							echo 'Erro ao executar query #10'. mysqli_error($link);
+							mysqli_rollback($link);
+						}
+
+						$getReservDetails = 'SELECT * FROM reserva WHERE idreserva ='.$idReserva.'';
+						$getDetails = mysqli_query($link, $getReservDetails);
+						if(!$getDetails)
+						{
+							echo 'Erro ao executar query #11'. mysqli_error($link);
+							mysqli_rollback($link);
+						}
+
+						$getDetails = mysqli_fetch_array($getDetails);
+
+						$corpoMsg = "Por favor comfirme a sua reserva através do endereço: ";
+						$corpoMsg .= "A sua nova password de acesso é ".$password." utilize a nossa àrea de login se desejar efetuar alterações. ";
+						$corpoMsg .= "A sua reserva está agendada para o dia ".$getDetails['data']." pelas ".$getDetails['hora']." Agradecemos a sua preferência.";
+
+						mailConfim('Reserva agendada.',$corpoMsg, $emailSendInfo);
+
+						mysqli_commit($link);
+						//header( "refresh:3;url=index.html" );
 					}
 				}
 			}
 			else
 			{
 				echo 'Reserva já se encontra validada.';
+				header( "refresh:3;url=index.html" );
 			}
 		}
 		else
 		{
-				echo 'Erro ao executar query #10'. mysqli_error($link);
+				echo 'Erro ao executar query #12'. mysqli_error($link);
 				die;
 		}
+	mysqli_close($link);
 ?>
--- PMS/form_reserva.php
+++ PMS/form_reserva.php
@@ -28,93 +28,116 @@ $numero = telefone($numero);
 
 $dateArray =converteDataHora($data_hora);
 
-$queryVerUser = 'SELECT * FROM cliente WHERE telefone = \''.$numero.'\'';
+$queryVerUser = 'SELECT * FROM cliente WHERE telefone = \''.$numero.'\' AND email = \''.$email.'\'';
 $verUser = mysqli_query($link,$queryVerUser);
 //Verificar se o utilizador já existe
 if(mysqli_num_rows($verUser) == 1)
 {
 	//verificar se ele tenta fazer reserva na mesma hora e no mesmo dia
-	$queryResAnteriores = 'SELECT * FROM reserva as r, cliente as c WHERE c.idcliente = r.cliente_idcliente AND r.hora = \''.$dateArray["hora"].'\' AND r.data = \''.$dateArray["data"].'\'';
-	$reservaAnteriores = mysqli_query($link,$queryResAnteriores);
-	if(!$reservaAnteriores)
+	$queryUserMail = 'SELECT email FROM cliente WHERE telefone = \''.$numero.'\'';
+	$resmail = mysqli_query($link,$queryUserMail);
+	$resmail = mysqli_fetch_array($resmail);
+	//Verifica se email introduzido match com o da base de dados.
+	if($resmail['email'] == $email)
 	{
-		echo 'Erro na query #1' . mysqli_error($link);
-		mysqli_rollback($link);
-		//die;
-	}
-	if(mysqli_num_rows($reservaAnteriores) > 0)
-	{
-		//falha a reserva
-		echo 'Já tem uma reserva para a mesma hora e para o mesmo dia.';
-	}
-	else
-	{	
-		//efetua a reserva && envia e-mail
-		if(!addReserva($link,$numero,$numPessoas,$numMesa,$dateArray["data"],$dateArray["hora"],''))
+		$queryResAnteriores = 'SELECT * FROM reserva as r, cliente as c WHERE c.idcliente = r.cliente_idcliente AND r.hora = \''.$dateArray["hora"].'\' AND r.data = \''.$dateArray["data"].'\'';
+		$reservaAnteriores = mysqli_query($link,$queryResAnteriores);
+		if(!$reservaAnteriores)
 		{
+			echo 'Erro na query #1' . mysqli_error($link);
 			mysqli_rollback($link);
+			//die;
+		}
+		if(mysqli_num_rows($reservaAnteriores) > 0)
+		{
+			//falha a reserva
+			echo 'Já tem uma reserva para a mesma hora e para o mesmo dia.';
 		}
 		else
-		{
-			mysqli_commit($link);
-			//Info from the user
-			$queryVerUser = 'SELECT c.idcliente FROM cliente AS c WHERE c.telefone = \''.$numero.'\'';
-			$getId = mysqli_query($link,$queryVerUser);
-			$idCli = mysqli_fetch_array($getId);
+		{	
+			//efetua a reserva && envia e-mail
+			if(!addReserva($link,$numero,$numPessoas,$numMesa,$dateArray["data"],$dateArray["hora"],''))
+			{
+				mysqli_rollback($link);
+			}
+			else
+			{
+				mysqli_commit($link);
+				//Info from the user
+				$queryVerUser = 'SELECT c.idcliente FROM cliente AS c WHERE c.telefone = \''.$numero.'\'';
+				$getId = mysqli_query($link,$queryVerUser);
+				$idCli = mysqli_fetch_array($getId);
 
-			//info from the reservation
-			$getIdRes = "SELECT idreserva FROM reserva WHERE hora = '".$dateArray["hora"]."' AND data ='".$dateArray["data"]."' AND cliente_idcliente=".$idCli['idcliente']."";		 
-			$getIdRes = mysqli_query($link,$getIdRes);
-			$idReserva = mysqli_fetch_array($getIdRes);
+				//info from the reservation
+				$getIdRes = "SELECT idreserva FROM reserva WHERE hora = '".$dateArray["hora"]."' AND data ='".$dateArray["data"]."' AND cliente_idcliente=".$idCli['idcliente']."";		 
+				$getIdRes = mysqli_query($link,$getIdRes);
+				$idReserva = mysqli_fetch_array($getIdRes);
 
-			$url = sprintf( 'id=%s&email=%s&numTel=%s&idRes=%s',$idCli['idcliente'] ,md5($email), md5($numero),$idReserva['idreserva']);
-			$corpoMsg = "Por favor comfirme a sua reserva através do endereço: ";
-			$corpoMsg .= sprintf("http://localhost/ativar.php?%s",$url);
-			
-			mailConfim('Confirmação de Reserva Dom Petisco.',$corpoMsg,$email);
-			//redirecionar para a página principal
+				$url = sprintf( 'id=%s&email=%s&numTel=%s&idRes=%s',$idCli['idcliente'] ,md5($email), md5($numero),$idReserva['idreserva']);
+				$corpoMsg = "Por favor comfirme a sua reserva através do endereço: ";
+				$corpoMsg .= sprintf("http://localhost/ativar.php?%s",$url);
+				
+				mailConfim('Confirmação de Reserva Dom Petisco.',$corpoMsg,$email);
+				//redirecionar para a página principal
+				header( "refresh:3;url=index.html" );
+			}
 		}
 	}
-
+	else
+	{	//email introduzido está mal pois n tá de acordo com o que está na bd
+		echo 'Verifique se introduziu os dados corretamente.';
+	}
 }
 else
 {
-	//registo um utilizador
-	if(!addCliente($link,$nome,$password,$numero,$apelido,$email))
-	{
-		echo 'Erro na query #2' . mysqli_error($link);
-		mysqli_rollback($link);
-		//die;
-	}
-	else
-	{
-		if(!addReserva($link,$numero,$numPessoas,$numMesa,$dateArray["data"],$dateArray["hora"],''))
+
+		$queryUserMail = 'SELECT email FROM cliente WHERE telefone = \''.$numero.'\'';
+		$resmail = mysqli_query($link,$queryUserMail);
+		if(mysqli_num_rows($resmail) == 0)
 		{
-			mysqli_rollback($link);
+			//registo um utilizador
+			if(!addCliente($link,$nome,$password,$numero,$apelido,$email))
+			{
+				echo 'Erro na query #2' . mysqli_error($link);
+				mysqli_rollback($link);
+				//die;
+			}
+			else
+			{
+				if(!addReserva($link,$numero,$numPessoas,$numMesa,$dateArray["data"],$dateArray["hora"],''))
+				{
+					mysqli_rollback($link);
+				}
+				else
+				{
+					mysqli_commit($link);
+					//Info from the user
+					$queryVerUser = 'SELECT c.idcliente FROM cliente AS c WHERE c.telefone = \''.$numero.'\'';
+					$getId = mysqli_query($link,$queryVerUser);
+					$idCli = mysqli_fetch_array($getId);
+
+					//info from the reservation
+					$getIdRes = "SELECT idreserva FROM reserva WHERE hora = '".$dateArray["hora"]."' AND data ='".$dateArray["data"]."' AND cliente_idcliente=".$idCli['idcliente']."";		 
+					$getIdRes = mysqli_query($link,$getIdRes);
+					$idReserva = mysqli_fetch_array($getIdRes);
+					//Prepara link de confirmação
+					$url = sprintf( 'id=%s&email=%s&numTel=%s&idRes=%s',$idCli['idcliente'] ,md5($email), md5($numero),$idReserva['idreserva']);
+					$corpoMsg = "Por favor confirme a sua reserva atraves do endereço: ";
+					$corpoMsg .= sprintf("http://localhost/ativar.php?%s",$url);
+					
+					//envia link de confirmação.
+					mailConfim('Confirmacao de Reserva Dom Petisco.',$corpoMsg,$email);
+					//rederecionar para a página principal.
+					header( "refresh:3;url=index.html" );
+				}
+
+			}
 		}
 		else
 		{
-			mysqli_commit($link);
-			//Info from the user
-			$queryVerUser = 'SELECT c.idcliente FROM cliente AS c WHERE c.telefone = \''.$numero.'\'';
-			$getId = mysqli_query($link,$queryVerUser);
-			$idCli = mysqli_fetch_array($getId);
-
-			//info from the reservation
-			$getIdRes = "SELECT idreserva FROM reserva WHERE hora = '".$dateArray["hora"]."' AND data ='".$dateArray["data"]."' AND cliente_idcliente=".$idCli['idcliente']."";		 
-			$getIdRes = mysqli_query($link,$getIdRes);
-			$idReserva = mysqli_fetch_array($getIdRes);
-			//Prepara link de confirmação
-			$url = sprintf( 'id=%s&email=%s&numTel=%s&idRes=%s',$idCli['idcliente'] ,md5($email), md5($numero),$idReserva['idreserva']);
-			$corpoMsg = "Por favor confirme a sua reserva atraves do endereço: ";
-			$corpoMsg .= sprintf("http://localhost/ativar.php?%s",$url);
-			
-			//envia link de confirmação.
-			mailConfim('Confirmacao de Reserva Dom Petisco.',$corpoMsg,$email);
-			//rederecionar para a página principal.
+			//Telefone deve estar mal pois o mail já existe na base de dados
+			echo 'Verifique se introduziu os dados corretamente.';
 		}
-
-	}
 }
 
 mysqli_close($link);
