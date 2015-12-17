949289c571aa03ef0444940544a50ee19d90c58b
paulorodrigues2 <p_asr@hotmail.com>
2015-12-17 16:31:43 +0000

    Ligsbf

--- PMS/funcionario/listar-funcionario.php
+++ PMS/funcionario/listar-funcionario.php
@@ -1,27 +1,35 @@
-<?php	
+<?php
 	require_once("../common/common.php");
 	require_once("../common/database.php");
-	
-	if(!isset($_SESSION['cliente_id']) || $_SESSION['cliente_id'] == '')
+	//require_once("../funcionario/login.php");
+	session_start();
+	if(empty($_SESSION['funcionario_id']))
 	{
 ?>
 		<p>Não tem autorização para aceder a esta página.</p>	
+		<?php header( "refresh:6;url=/login.php" ); ?>
 <?php	
 	}
 	else
 	{
-		if($_REQUEST["estado"] == "")
-		{
-			$query_reservas = "SELECT * FROM reservas";
-			$result_reservas = mysqli_query($query_reservas);
+		//if($_REQUEST["estado"] == "")
+		
+			$query_reservas = "SELECT * FROM reserva";
+			$result_reservas = mysqli_query($link, $query_reservas);
+			if(!$result_reservas)
+			{
+				echo mysqli_error($link);
+			}
 			
 			if (mysqli_num_rows($result_reservas) == 0)
 			{
-				echo "Não tem reservas para o dia de hoje";
+		echo "Não tem reservas para o dia de hoje";
+
 			}
 			else
 			{
-?>			
+?>
+			
 				<table>
 				 <thead>
 				  <tr>
@@ -36,52 +44,30 @@
 			 
 				 <tbody>
 <?php
-			
-					//$query_comp_type = "SELECT id, name FROM comp_type GROUP BY name";
-					//$result_comp_type = mysql_query($query_comp_type);
-			
-					
-						$query_listareserva = "SELECT * FROM cliente, reserva, reserva_has_mesa, mesa,
-				WHERE reserva.cliente.id=cliente.id and reserva.ativo='1',
-								GROUP BY reserva.date and reserva.time";
+						$query_listareserva = "SELECT DISTINCT * FROM cliente, reserva, reserva_has_mesa, mesa WHERE reserva.cliente_idcliente=cliente.idcliente and reserva_has_mesa.reserva_idreserva=reserva.idreserva  and reserva_has_mesa.mesa_numero=mesa.numero and reserva.ativo='1'";
 														
-						$result_listareserva = mysqli_query($query_listareserva);
-				
+						$result_listareserva = mysqli_query($link, $query_listareserva);
+				if(!$result_listareserva)
+			{
+				echo mysqli_error($link);
+			}
 						
 						
-							while($array_listareserva = mysql_fetch_array($result_listareserva))
+							while($array_listareserva = mysqli_fetch_array($result_listareserva))
 							{
-?>
-								<td> <?php echo $array_listareserva["reserva.id"] ?></td>
-								<td> <?php echo $array_listareserva["cliente.nome"] ?> </td>
-								<td> <?php echo $array_listareserva["reserva.data"] ?></td>
-								<td> <?php echo $array_listareserva["reserva.hora"] ?></td>
-								<td> <?php echo $array_listareserva["mesa.num_pessoas"] ?></td>
-								<td> <?php echo $array_listareserva["mesa.id"] ?></td>
-<?php						
-								if($array_listareserva["state"] == "active")
-								{
-?>							
-									<td>ativo</td>
-									<td>[editar] [desativar]</td>
-<?php
-								}
-								else
-								{
-?>							
-								<td>inativo</td>
-								<td>[editar] [desativar]</td>
-<?php
-								}							
-?>
-							</tr>
-<?php						}									
-						}				
-					}			
-?>
-				 </tbody>
-				</table>			
-<?php					
+?>								<tr>
+								<td> <?php echo $array_listareserva["idreserva"] ?></td>
+								<td> <?php echo $array_listareserva["nome"] ?> </td>
+								<td> <?php echo $array_listareserva["data"] ?></td>
+								<td> <?php echo $array_listareserva["hora"] ?></td>
+								<td> <?php echo $array_listareserva["capacidade"] ?></td>
+								<td> <?php echo $array_listareserva["numero"] ?></td>
+					           </tr>
+<?php							
+}
+            
+				}
 			}
 ?>			
-			
+
+								
